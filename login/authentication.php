b7ec06b13a534a5e12c9718e338f27dad57970ca
jernber <oraclemars@gmail.com>
2019-03-17 18:06:18 -0500

    authentication page takes email, not usernames

--- login/authentication.php
+++ login/authentication.php
@@ -4,27 +4,26 @@
     session_start();
     
     if($_POST){
-        $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         $passAttempt = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        $sql = "SELECT * from cms_users";
-        //$sql = "SELECT id, Username, password FROM cms_users WHERE Username = :username";
+        // $sql = "SELECT * from cms_users";
+        $sql = "SELECT id, Username, password, email FROM cms_users WHERE email = :email";
         $stmt = $db->prepare($sql);
-        
-        $stmt->bindValue(':username', $username);
+        $stmt->bindValue(':email', $email);
         $stmt->execute();
        
-        // $user = $stmt->fetch(PDO::FETCH_ASSOC);
-        // if($user == false){
-        //     die('Incorrect username / password combination! 1');
-        // } else {
-        //     $password = password_verify($passAttempt, $user['password']);
-        //     if($password){
-        //         $_SESSION['user_id'] = $user['id'];
-        //         $_SESSION['logged_in'] = time();
-        //         header(SITE_ROOT . 'index.php');
-        //     } else {
-        //         die('Incorrect username / password combination! 2');
-        //     }
-        // }
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+        if($user == false){
+            die('Incorrect email / password combination! 1');
+        } else {
+            $password = password_verify($passAttempt, $user['password']);
+            if($password){
+                $_SESSION['user_id'] = $user['id'];
+                $_SESSION['logged_in'] = time();
+                header(SITE_ROOT . 'index.php');
+            } else {
+                die('Incorrect email / password combination! 2');
+            }
+        }
     }
 ?>
